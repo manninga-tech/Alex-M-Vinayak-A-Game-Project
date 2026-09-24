25a35878094f0ee2242f2da0165e1fe771f42d23
manninga-tech <manninga@wsc.school.nz>
2026-09-24 12:29:08 +1200

    Started working on basic dash

--- new-game-project/scripts/character_body_2d.gd
+++ new-game-project/scripts/character_body_2d.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -220.0
 
+@onready var dash_cooldown : Timer = $Timer
 
 func _physics_process(delta: float) -> void:
+	
+	var can_dash : bool = true
+	
+	if Input.is_action_just_pressed("dash") and can_dash and is_on_floor():
+		print("dash")
+	
+
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -21,5 +30,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	
 
 	move_and_slide()
